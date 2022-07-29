043c78e4df213d991f60495afcabe9a58bea44fc
MdMobarokHossain <mhparvejchrf@gmail.com>
2022-07-29 23:25:08 +0600

    Sequence extraction R file

---
+++ extract_specific_region_from_multifasta_file.r
@@ -0,0 +1,17 @@
+#!/usr/bin/env Rscript
+
+library(seqinr)
+
+seqs <- read.fasta("final.fasta")
+locs <- read.delim("final.coords", header=F, sep=" ") # Tabs? Put sep="\t"
+outf <- file("a001-results.fasta", 'w')
+
+doitall <- function(x) {
+  seq_id <- x[[1]]; start <- x[[2]]; stop <- x[[3]]; seq <- seqs[[seq_id]]
+  seqv   <- seq[start:stop]
+  header <- paste(sep="", ">", attr(seq, "name"), "_", start, "_", stop, "\n")
+  cat(file=outf, header)
+  cat(file=outf, toupper(paste(sep="", collapse="", seqv)), "\n")
+}
+apply(locs, 1, doitall)
+close(outf)
